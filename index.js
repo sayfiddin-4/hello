6a4b3676336ebe8ffae61be65fe5a2cec98130cb
sayfiddin-4 <sayfiddinsadikov5@gmail.com>
2023-11-07 16:40:31 +0300

    hello commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// var str1 = 'Web'
-// var str2 = 'Brain'
-// var str3 = ''
-// str3=str1
-// str1=str2
-// str2=str3
+var str1 = 'Web'
+var str2 = 'Brain'
+var str3 = ''
+str3=str1
+str1=str2
+str2=str3
 
-// console.log(str1,str2);
+console.log(str1,str2);
 
 
 var yil = 2020
