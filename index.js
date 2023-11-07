3ee8e751ca4775bfb99627d67af6ba0fc30162e1
sayfiddin-4 <sayfiddinsadikov5@gmail.com>
2023-11-07 16:34:06 +0300

    Sayfiddin commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// var str1 = 'Web'
+// var str2 = 'Brain'
+// var str3 = ''
+// str3=str1
+// str1=str2
+// str2=str3
+
+// console.log(str1,str2);
+
+
+var yil = 2020
+
+var yil = 2017
+
+const Year = (yil) => {
+    if (yil = '2020') {
+        console.log('kabisa yili');
+    }
+    else {
+        console.log('kabisa yili emas');
+    }
+}
+
+console.log(Year(yil));
