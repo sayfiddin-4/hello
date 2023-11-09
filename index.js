3c8f1da1df4bad6f3847a7d8a7b58468b7f70cfb
sayfiddin-4 <sayfiddinsadikov5@gmail.com>
2023-11-09 15:43:13 +0300

    goodbye

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-var str1 = 'Web'
-var str2 = 'Brain'
-var str3 = ''
-str3=str1
-str1=str2
-str2=str3
-
-console.log(str1,str2);
-
-
-var yil = 2020
-
-var yil = 2017
-
-const Year = (yil) => {
-    if (yil = '2020') {
-        console.log('kabisa yili');
-    }
-    else {
-        console.log('kabisa yili emas');
-    }
+const KabisYear = (year) => {
+    return year % 4 == 0 ? 'Kabis Yili' : 'Kabisa Yili emas'
 }
 
-console.log(Year(yil));
+console.log(KabisYear(2024));
+
+const toUpperCase = (name) => {
+    let res = name.split('')
+    resalt = res.filter((val) => {
+        return val == val.toUpperCase()
+    }).join('')
+    return resalt
+}
+
+const name = 'WebBraInAcadeMy'
+console.log(toUpperCase(name));
